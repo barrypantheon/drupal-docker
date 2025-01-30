f0ac5be5792d0210ae17dac116a3fb2a99fb4bfa
barrypantheon <101330553+barrypantheon@users.noreply.github.com>
2025-01-30 18:34:21 +0000

    Create settings.local.php

---
+++ settings.local.php
@@ -0,0 +1,15 @@
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'drupal11',
+  'username' => 'drupal11',
+  'password' => 'drupal11',
+  'prefix' => '',
+  'host' => 'database',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+
+$settings['config_sync_directory'] = '../config';
+$settings['hash_salt'] = '_gxG2LpNTg1y1tFns2g6DwB0UA44224aUWzBcVoLcP8QG1bqjLpamALu4ft4HOKE6GUNk9rpYQ';
